d32a8950efadb971539e41c74c532ec527173ec6
cylim76 <cylim76@gmail.com>
2022-12-11 20:15:22 +0800

    Create 03_headlessmode.py

---
+++ 2022/03_headlessmode.py
@@ -0,0 +1,11 @@
+
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+# # 使用headless 模式只需给webdriver 添加 options 参数即可， headless 模式下剪贴板无法操作！
+chrome_options = Options()
+chrome_options.headless = True
+chrome_options.add_argument("window-size=1920x1080")
+chrome_options.add_argument("User-Agent = Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+browser  = webdriver.Chrome(options=chrome_options)
