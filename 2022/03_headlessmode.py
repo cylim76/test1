3856bf15ce016b9a6cff12857838e84afa0b2ff2
cylim76 <cylim76@gmail.com>
2022-12-22 11:32:03 +0800

    Update 03_headlessmode.py

--- 2022/03_headlessmode.py
+++ 2022/03_headlessmode.py
@@ -8,4 +8,5 @@ chrome_options = Options()
 chrome_options.headless = True
 chrome_options.add_argument("window-size=1920x1080")
 chrome_options.add_argument("User-Agent = Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+chrome_options.add_argument("log-level=3") # 终端不显示 错误提示 SSL error code 1
 browser  = webdriver.Chrome(options=chrome_options)
